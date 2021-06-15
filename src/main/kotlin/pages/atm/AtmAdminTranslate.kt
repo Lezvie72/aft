89971660fd7805459eb11a36377e28496094b358
Mark <mark.seregin@diabolocom.com>
2021-06-15 13:16:36 +0300

    5510 временная

---
+++ src/main/kotlin/pages/atm/AtmAdminTranslate.kt
@@ -0,0 +1,8 @@
+package pages.atm
+
+import org.openqa.selenium.WebDriver
+import pages.core.annotations.PageUrl
+
+@PageUrl("/translate")
+class AtmAdminTranslate(driver: WebDriver) : AtmAdminPage(driver) {
+}
