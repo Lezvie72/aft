3312742f54ef972b98f01e752e22d920086e7274
Mark Seregin Notebook <seregin.mi@unicom24.ru>
2021-06-18 14:26:12 +0300

    Фикс

--- src/main/kotlin/pages/atm/AtmAdminKibanaPage.kt
+++ src/main/kotlin/pages/atm/AtmAdminKibanaPage.kt
@@ -1,9 +1,18 @@
 package pages.atm
 
 import org.openqa.selenium.WebDriver
+import pages.BasePage
+import pages.core.annotations.PageDomain
 import pages.core.annotations.PageUrl
 
-@PageUrl("/kibana.dev.n-t.io")
-class AtmAdminKibanaPage(driver: WebDriver): AtmAdminPage(driver) {
+@PageDomain(
+    baseUrlProperty = "{тута должен быть базовый урл}.url",
+    baseUrlLoginProperty = "{тута должен быть базовый урл}.login",
+    baseUrlPasswordProperty = "{тута должен быть базовый урл}.password",
+    authProvider = AtmAdminLoginPage::class
+)
+
+@PageUrl("а вот тута примочка")
+class AtmAdminKibanaPage(driver: WebDriver): BasePage(driver) {
 
 }
